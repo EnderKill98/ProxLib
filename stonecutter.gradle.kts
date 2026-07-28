13aaa493e7e4c131eabd7ccf04079f47e6171476
Shayne Hartford <shaybox@shaybox.com>
2026-07-28 05:42:41 -0400

    Add 26.2

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -4,4 +4,4 @@ plugins {
     //id("dev.kikugie.j52j") version "1.0.2" apply false // Enables asset processing by writing json5 files
     //id("me.modmuss50.mod-publish-plugin") version "0.7.+" apply false // Publishes builds to hosting websites
 }
-stonecutter active "26.1" /* [SC] DO NOT EDIT, oops */
+stonecutter active "26.2" /* [SC] DO NOT EDIT, oops */
